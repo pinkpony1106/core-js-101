1939e49b6ae202249db3aba7e6b77d9bac891372
pinkpony1106 <juliaegorova221098@gmail.com>
2023-06-15 18:30:46 +0300

    feat: added solutions to promises-tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -5,7 +5,6 @@
  *                                                                                                *
  ************************************************************************************************ */
 
-
 /**
  * Return Promise object that is resolved with string value === 'Hooray!!! She said "Yes"!',
  * if boolean value === true is passed, resolved with string value === 'Oh no, she said "No".',
@@ -28,10 +27,18 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
-}
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, rejected) => {
+    if (isPositiveAnswer === true) {
+      return resolve('Hooray!!! She said "Yes"!');
+    }
+    if (isPositiveAnswer === false) {
+      return resolve('Oh no, she said "No".');
+    }
 
+    return rejected(new Error('Wrong parameter is passed! Ask her again.'));
+  });
+}
 
 /**
  * Return Promise object that should be resolved with array containing plain values.
@@ -48,8 +55,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +78,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
@@ -92,8 +99,23 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  const resolved = [];
+  const rejected = [];
+
+  return new Promise((resolve) => {
+    array.forEach(async (promise) => {
+      try {
+        const result = await promise;
+        resolved.push(result);
+        if (resolved.length + rejected.length === array.length) {
+          resolve(resolved.reduce(action));
+        }
+      } catch (elem) {
+        rejected.push(new Error(elem));
+      }
+    });
+  });
 }
 
 module.exports = {
